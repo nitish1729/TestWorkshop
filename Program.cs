3ae6c5cd2988d0dd98fff60ddee8286e39ef70ed
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-13 10:56:55 +0530

    test Work question

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+Console.WriteLine("Enter the first string");
+string anagram1 = Console.ReadLine();
+Console.WriteLine("Enter the 2nd string");
+string anagram2 = Console.ReadLine();
+
+ if( anagram1 == anagram2)
+ {
+    Console.WriteLine("Anagram1 is equal to Anagram2 ");
+ }
+  else
+  {
+    Console.WriteLine("Anagram1 is not equal to Anagram2");
+  }
+  Console.ReadLine();
